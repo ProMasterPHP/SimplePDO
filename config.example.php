7b0b894fd9471851ac1fa353e7d33113ebc317c4
ProMasterPHP <husanboyevich504@gmail.com>
2024-04-19 12:42:01 +0500

    create file

---
+++ config.example.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'dbtype' => 'mysql',
+    'dbuser' => '', //username
+    'dbpass' => '', //password
+
+    'dsn' => [
+        'host' => 'localhost',
+        'port' => 3306,
+        'charset' => 'utf8mb4',
+        'dbname' => '', // Database name
+    ]
+];
+?>
