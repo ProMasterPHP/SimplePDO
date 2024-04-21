600e031049f1eeeb8322e8cdd8c606fef3415564
ProMasterPHP <husanboyevich504@gmail.com>
2024-04-21 08:05:49 +0500

    update config.php

--- config.php
+++ config.php
@@ -5,10 +5,16 @@ Autoloader::boot(Autoloader::find('Model'));
 
 use TurgunboyevUz\SimplePDO\Database;
 
+$sql = [
+    'dbuser'=>'', // your database username
+    'dbpass'=>'', // your database password
+    'dbname'=>'' // your database name
+];
+
 Database::connect('mysql', [
     'host' => 'localhost',
     'port'=>3306,
     'charset' => 'utf8mb4',
-    'dbname' => 'codearch'
-], 'root', 'OnlineWolf2003');
+    'dbname' => $sql['dbname'],
+], $sql['dbuser'], $sql['dbpass']);
 ?>
